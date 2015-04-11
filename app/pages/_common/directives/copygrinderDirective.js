5c52064f9b24bbf975f97d549ff261afe147ed18
Cameron J. Ross <contact@cameronjross.com>
2015-04-11 12:49:54 -0400

    Created basic directive and service.

--- app/pages/_common/directives/copygrinderDirective.js
+++ app/pages/_common/directives/copygrinderDirective.js
@@ -1,7 +1,39 @@
 'use strict';
 
-angular.module('copygrinderHome').directive('copygrinder', function (copygrinderService) {
-  return {
-    restrict: 'AE'
+(function () {
+
+  var buildQueryDirective = function (name, dataFunc) {
+
+    angular.module('copygrinderHome').directive(name, function (copygrinderService) {
+
+      var cgDirective = {};
+
+      cgDirective.restrict = 'AE';
+
+      cgDirective.link = function ($scope, $element, $attr) {
+        var cgQuery = $attr[name];
+        if (cgQuery) {
+          var match = cgQuery.match(/^\s*(.+)\s+as\s+(.+)\s*$/);
+          if (match) {
+            var query = match[1];
+            var scopeVar = match[2];
+            copygrinderService.query(query, function (data) {
+              $scope[scopeVar] = dataFunc(data);
+            });
+          }
+        }
+      };
+
+      return cgDirective;
+    });
   };
-});
+
+  buildQueryDirective('cgQuery', function (data) {
+    return data;
+  });
+
+  buildQueryDirective('cgSingletonQuery', function (data) {
+    return data[0].content;
+  });
+
+})();
--- app/pages/_common/services/copygrinder-service.js
+++ app/pages/_common/services/copygrinder-service.js
@@ -1,8 +1,14 @@
 'use strict';
 
-angular.module('copygrinderHome').factory('CopygrinderService', function () {
+angular.module('copygrinderHome').factory('copygrinderService', function ($http) {
 
   var cgService = {};
+
+  var rootUrl = document.body.getAttribute('data-copygrinder-url');
+
+  cgService.query = function(queryString, callback) {
+    $http.get(rootUrl + 'copybeans?' + queryString).success(callback);
+  };
 
   return cgService;
 });
